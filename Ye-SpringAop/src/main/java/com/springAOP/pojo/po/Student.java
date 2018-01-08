9322edaf365ab0dc902bcd546b5bd858a96d118f
Administrator <Administrator@SKY-20170210AWB>
2018-01-08 13:07:04 +0800

    初始化

---
+++ Ye-SpringAop/src/main/java/com/springAOP/pojo/po/Student.java
@@ -0,0 +1,16 @@
+package com.springAOP.pojo.po;
+
+public class Student {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
